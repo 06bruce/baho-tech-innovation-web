a48a3bd6c628545216b1a7bf0fb7ca88c27e8b6a
bruce <brucenshuti2@gmail.com>
2026-07-26 16:50:00 +0200

    refactor: remove legacy backend files and refresh frontend build assets

--- backend/src/modules/theme/theme.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { themeOptions } from "./theme.controller.js";
-
-export const themeRoutes = Router();
-
-themeRoutes.get("/options", themeOptions);
--- backend/src/modules/translation/translation.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { languageOptions, translateDynamicContent } from "./translation.controller.js";
-
-export const translationRoutes = Router();
-
-translationRoutes.get("/languages", languageOptions);
-translationRoutes.post("/translate", translateDynamicContent);
